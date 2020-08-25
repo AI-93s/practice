a61ed56925348c786bdcba0c9fa5447c95be7fe3
AI-93s <gkasperg@mail.ru>
2020-08-25 21:44:46 +0300

    Lesson017

--- js/script.js
+++ js/script.js
@@ -221,3 +221,32 @@
 //     console.log('1');
 //     return a + b;
 // };
+
+// Lesson 017
+
+// const str = "test";
+
+// console.log(str.length);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+// let fruit = "Some fruit";
+
+// console.log(fruit.indexOf("fruit"));
+
+// const logg = "Hellow world";
+
+// console.log(logg.slice(6 ));
+
+// console.log(logg.substring(6, 11));
+
+// console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
