71154f2aae37d20d191ae17a2403e46eb5261721
AI-93s <gkasperg@mail.ru>
2020-08-26 17:32:21 +0300

    023

--- js/script.js
+++ js/script.js
@@ -50,8 +50,8 @@
 // let incr = 10,
 //     decr = 10;
 
-// // incr++;
-// // decr--;
+// incr++;
+// decr--;
 
 // console.log(++incr);
 // console.log(--decr);
@@ -60,8 +60,8 @@
 
 // console.log(2 + 2 * 2 !== '6');
 
-// // &&
-// // ||
+// &&
+// ||
 
 // const isChecked = false,
 //     isClose = false;
@@ -141,7 +141,7 @@
 
 // for (let i = 1; i < 8; i++) {
 //     if (i === 6) {
-//         // break;
+//         break;
 //         continue;
 //     }
 //     console.log(i);
@@ -238,7 +238,7 @@
 
 // const logg = "Hellow world";
 
-// console.log(logg.slice(6 ));
+// console.log(logg.slice(6));
 
 // console.log(logg.substring(6, 11));
 
@@ -258,7 +258,8 @@
 // function start() {
 //     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//     while (numberOfFilms == '' || numberOfFilms == null ||
+//         isNaN(numberOfFilms)) {
 //         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 //     }
 // }
@@ -310,7 +311,8 @@
 
 // function writeYourGenres() {
 //     for (let i = 1; i < 4; i++) {
-//         personaMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`);
+//         personaMovieDB.genres[i - 1] =
+//             prompt(`Ваш любимый жанр под номером ${i}?`);
 //     }
 
 // }
@@ -324,7 +326,7 @@
 // Lesson 019
 
 // function first() {
-//     // Do somethings
+//     Do somethings
 //     setTimeout(function () {
 //         console.log(1);
 //     }, 500);
@@ -337,13 +339,196 @@
 // first();
 // second();
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang} `);
-    callback();
-}
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang} `);
+//     callback();
+// }
 
-function done() {
-    console.log('Я прошел этот урок!');
-}
+// function done() {
+//     console.log('Я прошел этот урок!');
+// }
 
-learnJS('JavaScript', done);
+// learnJS('JavaScript', done);
+
+// Lesson 020
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         background: 'red'
+//     },
+//     makeTest: function () {
+//         console.log("Test");
+//     }
+// };
+
+// options.makeTest();
+
+// const {
+//     border,
+//     background
+// } = options.colors;
+// console.log(border);
+
+// console.log(Object.keys(options).length);
+
+// console.log(options["colors"]["border"]);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof (options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+// Lesson 021
+
+// const arr = [2, 12, 26, 8, 10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+// arr.forEach(function (item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// arr[99] = 0;
+
+// console.log(arr.length);
+
+// arr.pop();
+
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
+
+// Lesson 022
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; // Ссылка
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     for (let key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'ddddddd';
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//     blogs = ['wordpress', 'livejournal', 'blogger'],
+//     internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// console.log(...num);
+
+// const array = ["a", "b"];
+
+// const newAarray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// }
+
+// const newObj = {
+//     ...q
+// };
+
+// Lesson 023
