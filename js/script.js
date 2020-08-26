2a849fd4637e2c5f1311fa9475ec1c24ecbd4c77
AI-93s <gkasperg@mail.ru>
2020-08-26 23:41:58 +0300

    a

--- js/script.js
+++ js/script.js
@@ -632,4 +632,51 @@
 
 // console.log(personaMovieDB);
 
-// Lesson 025
+// Lesson 026
+
+//1
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+//2
+console.log(typeof(5 + ''));
+
+const num = 5;
+
+console.log("htttps://vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+//1
+console.log(typeof(Number('4')));
+
+//2
+console.log(typeof(+'5'));
+
+//3
+console.log(typeof(parseInt("15px", 10)));
+
+let answ = +prompt("Hello", "");
+
+//1
+
+// 0, '', null, undefined, NaN;
+
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+//2
+console.log(typeof(Boolean('4')));
+
+//3
+console.log(typeof(!!"ssss"));
+
