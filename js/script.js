1b0ebeb806f98a29c64b04fc72a637e9fc2595d4
AI-93s <gkasperg@mail.ru>
2020-09-03 19:56:58 +0300

    aaaaa

--- js/script.js
+++ js/script.js
@@ -789,3 +789,108 @@
 // div.textContent = "Hello";
 
 // div.insertAdjacentHTML("", '<h2>Hello</h2>');
+
+// Lesson 031
+
+// const btns = document.querySelectorAll('button'),
+//     overlay = document.querySelector('.overlay');
+// btn.onclick = function () {
+//     alert('Click');
+// };
+
+// btn.onclick = function () {
+//     alert('Second click');
+// };
+
+// let i = 0;
+// const deleteElement = (e) => {
+//     console.log(e.target);
+//     console.log(e.type);
+//     i++;
+//     if (i == 1) {
+//         btn.removeEventListener('click', deleteElement);
+//     }
+// };
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', deleteElement, {once: true});
+// });
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+// const link = document.querySelector('a');
+
+// link.addEventListener('click', function (event) {
+//     event.preventDefault();
+
+//     console.log(event.target);
+// });
+
+// Lesson 032
+
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.lastElementChild);
+
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+// for (let node of document.body.childNodes) {
+//     if (node.nodeName == '#text') {
+//         continue;
+//     }
+
+//     console.log(node);
+
+// }
+
+// Lesson 034
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const box = document.querySelector('.box');
+
+//     box.addEventListener('touchstart', (e) => {
+//         e.preventDefault();
+
+//         console.log('Start');
+//         console.log(e.touches);
+//     });
+
+//     box.addEventListener('touchmove', (e) => {
+//         e.preventDefault();
+
+//         console.log(e.targetTouches[0].pageX);
+//     });
+
+//     box.addEventListener('touchend', (e) => {
+//         e.preventDefault();
+
+//         console.log('End');
+//     });
+// });
+
+// touches
+// targetTouches
+// changedTouches
+
+// Lesson 035
+
+// const p = document.querySelectorAll('p');
+// console.log(p);
+
+// function loadScript(src) {
+//     const script = document.createElement('script');
+//     script.src = src;
+//     script.async = false;
+//     document.body.append(script);
+// }
+
+// loadScript("js/test.js");
+// loadScript("js/some.js");
